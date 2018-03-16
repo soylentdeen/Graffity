9add3c5b2a62ec2881acdc5aec07e58d9ff4fb6b
Casey Deen <cdeen@mpe.mpg.de>
2018-03-16 12:41:50 +0100

    modified PlotTools

--- src/PlotTools.py
+++ src/PlotTools.py
@@ -1,10 +1,17 @@
-from matplotlib import pyplot
+from matplotlib import pyplot, _pylab_helpers
+
+def clearAllPlots():
+    #figures = [manager.canvas.figure for manager in _pylab_helpers.Gcf.get_all_fig_managers()]
+    #for f in figures:
+    #    f.close()
+    pyplot.close('all')
 
 def configurePlots(number):
     figures = []
     axes = []
+    nfigs = len(_pylab_helpers.Gcf.get_all_fig_managers())
     for i in range(number):
-        figures.append(pyplot.figure(i))
+        figures.append(pyplot.figure(i+nfigs))
         figures[-1].clear()
         axes.append(figures[-1].add_axes([0.1, 0.1, 0.8, 0.8]))
         axes[-1].clear()
